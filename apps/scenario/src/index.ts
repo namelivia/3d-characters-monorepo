8876e48c608a7bcbca0886f37c02d524e851d3f0
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2023-08-13 14:23:54 +0200

    Initial implementation of dialog boxes

--- apps/scenario/src/index.ts
+++ apps/scenario/src/index.ts
@@ -1,9 +1,23 @@
 import { loadGLTF } from './gltf/loader'
 import { loadScene } from './scene/loader'
 import World from './world/world'
+import Dialog from './dialog/dialog'
 
 const main = async () => {
 	const world = new World()
+	const dialog = new Dialog()
+	dialog.addToDOM()
+	setTimeout(() => {
+		dialog.setText('Hello world!')
+		dialog.setVisible(true)
+	}, 5000)
+	setTimeout(() => {
+		dialog.setText("This is just a test. Don't be too harsh on me.")
+	}, 10000)
+	setTimeout(() => {
+		dialog.setText('')
+		dialog.setVisible(false)
+	}, 15000)
 	world.initialize()
 	const gltf = await loadGLTF()
 	const scene = await loadScene(gltf, 'scene1')

fe0cbe9627b9ec21a7d526442f16df1603faf971
José Ignacio Amelivia Santiago <hello@namelivia.com>
2024-09-23 00:57:24 +0200

    Quick fix

--- apps/scene_editor/src/ui/result/json_preview.ts
+++ apps/scene_editor/src/ui/result/json_preview.ts
@@ -1,16 +1,24 @@
 import { BasicSceneJSON } from 'common'
 
 class JsonPreview {
+	clear() {
+		const previous = document.getElementById('json-preview')
+		if (previous) {
+			previous.remove()
+		}
+	}
 	display(scene: BasicSceneJSON) {
 		const result = document.getElementById('result')
-        if (result) {
-		    const json = JSON.stringify(scene, null, 2)
-            const pre = document.createElement('pre')
-            if (pre) {
-                pre.innerHTML = json
-            }
-            result.appendChild(pre)
-        }
+		this.clear()
+		if (result) {
+			const json = JSON.stringify(scene, null, 2)
+			const pre = document.createElement('pre')
+			pre.setAttribute('id', 'json-preview')
+			if (pre) {
+				pre.innerHTML = json
+			}
+			result.appendChild(pre)
+		}
 	}
 }
 
